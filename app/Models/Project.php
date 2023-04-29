9e796344127d9470782ab012f3d9167b92d9a27d
batuhanarslan <batuhan.ars@yahoo.com>
2023-04-29 18:32:29 +0300

    Proje migration, model, controller ve resource dosyaları hazır.

---
+++ app/Models/Project.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
+class Project extends Model
+{
+    use HasFactory, HasSlug;
+
+    protected $fillable = ["title", "slug", "logo"];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
+}
